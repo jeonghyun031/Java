23a4a00e32b9c0b92cd13e0fa098342839b0f59e
jeonghyun031 <pjh1849@gmail.com>
2024-10-30 15:03:03 +0900

    add java
    프로그래밍언어구조론 실습 코드.
    자바에서의 자료형을 연습해보자

---
+++ java/type.java
@@ -0,0 +1,24 @@
+// JAVA 언어에서의 자료형
+public class Hello {
+    public static int sum(int i, int j) {
+        return i + j;
+    }
+
+    public static void main(String[] args) {
+        int i;
+        int j;
+        char a;
+        String b;
+        final int TEN = 10;
+
+        i = 1;
+        j = sum(i,TEN);
+        a = '?';
+        b = "Hello";
+
+        java.lang.System.out.println(a);
+        System.out.println(b);
+        System.out.println(TEN);
+        System.out.println(j);
+    }
+}

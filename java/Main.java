13174218ac96e8f95758c3240f734dd2f26f14ee
jeonghyun031 <pjh1849@gmail.com>
2024-11-05 15:28:08 +0900

    add java
    PLST 5개의 원소를 가지는 정수형 1차원배열을 자바로 구현해보기

---
+++ java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i, a[] = new int[5];
+        for(i = 0; i < 5; i++) {
+            a[i] = (i+1) * 10;
+            System.out.print(a[i] + " ");
+        }
+    }
+}
